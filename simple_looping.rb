3f740383a82108b98531e5624b4763ec6613630f
Yolanda Galdamez <yolanda.v.galdamez@gmail.com>
2018-03-07 05:09:46 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,17 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  counter = 0
+  
+  loop do
+    counter += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if counter >= 7
+      break 
+    end
+  end
+  
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -13,12 +24,21 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
+  7.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
 
 def while_iterator(number_of_times)
+  counter = 0 
+  while counter < 7
+    puts "Welcome to Flatiron School's Web Development Course!"
+  counter += 1 
+  end
+  
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -26,6 +46,13 @@ def while_iterator(number_of_times)
 end
 
 def until_iterator(number_of_times)
+  
+  counter = 0 
+  until counter == 7
+    puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1 
+  end
+    
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -33,10 +60,15 @@ def until_iterator(number_of_times)
 end
 
 def for_iterator(number_of_times)
+  number_of_times = 1..7
+  
+  for looping in number_of_times
+     puts "Welcome to Flatiron School's Web Development Course!"
+  end
+  
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
 end
-
